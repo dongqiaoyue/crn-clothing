4686f8e37daf77512e60a6b4c19965d5344a18ea
dongqiaoyue <dongqiaoyue98@gmail.com>
2022-12-05 17:14:08 -0500

    routes

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import {Link, Outlet} from "react-router-dom";
+import {Fragment} from 'react';
+import {ReactComponent as NeuLogo } from "../assets/Northeastern_Wordmark.svg";
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <NeuLogo className='logo' />
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    );
+}
+
+export default Navigation;
