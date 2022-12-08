676c0daee7a592448f069492f7324b77fe77786f
dongqiaoyue <dongqiaoyue98@gmail.com>
2022-12-08 17:14:01 -0500

    cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./routes/navigation/navigation.component";
 import {Route, Routes} from 'react-router-dom';
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
                 <Route index element={<Home />}></Route>
                 <Route path='shop' element={<Shop />}></Route>
                 <Route path='auth' element={<Authentication />}></Route>
+                <Route path='checkout' element={<Checkout />}></Route>
             </Route>
         </Routes>
     );
